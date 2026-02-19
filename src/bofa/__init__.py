d12a6ca1e490036d320dc35cf4cd0c261c36dff1
Sean O'Brien <60306702+stobrien89@users.noreply.github.com>
2026-02-19 14:15:31 -0500

    feat: add binary support (#2)

--- src/bofa/__init__.py
+++ src/bofa/__init__.py
@@ -2,6 +2,8 @@ import importlib.metadata
 
 __version__ = importlib.metadata.version("bofa")
 
+BOFA = b'\x42\x6f\x66\x61\x20\x64\x65\x65\x7a\x20\x6e\x75\x74\x73'
+
 
 def main():
-    print("Bofa deez nuts")
+    print(BOFA.decode('utf-8'))
